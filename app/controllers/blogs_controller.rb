5a2124627d1284c589f83ae125689761a9c96786
Ami-tnk <ami1y251992@gmail.com>
2020-11-18 06:31:37 +0000

    [Finish]3-6 編集機能の作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
-    redirect_to blog_path(blog)
+    redirect_to blog_path(blog.id)
   end
 
   private
